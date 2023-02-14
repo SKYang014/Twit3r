7653427bb5ee770a4f32b09a5f843bf5e7bc50a9
poofy <skyang014@gmail.com>
2023-02-13 19:57:13 -0800

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+
+import { trpc } from "../utils/trpc";
+
+import "../styles/globals.css";
+import { Container } from "../components/Container";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Container>
+        <main>
+          <Component {...pageProps} />
+        </main>
+      </Container>
+    </SessionProvider>
+  );
+};
+
+export default trpc.withTRPC(MyApp);
---
+++ src/server/trpc/trpc.ts
@@ -0,0 +1,41 @@
+import { initTRPC, TRPCError } from "@trpc/server";
+import superjson from "superjson";
+
+import { type Context } from "./context";
+
+const t = initTRPC.context<Context>().create({
+  transformer: superjson,
+  errorFormatter({ shape }) {
+    return shape;
+  },
+});
+
+export const router = t.router;
+
+/**
+ * Unprotected procedure
+ * these routes are open to the public and not authenticated
+ **/
+export const publicProcedure = t.procedure;
+
+/**
+ * Reusable middleware to ensure
+ * users are logged in
+ */
+const isAuthed = t.middleware(({ ctx, next }) => {
+  if (!ctx.session || !ctx.session.user) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+  return next({
+    ctx: {
+      // infers the `session` as non-nullable
+      session: { ...ctx.session, user: ctx.session.user },
+    },
+  });
+});
+
+/**
+ * Protected procedure
+ * routes where we require users to be authenicated
+ **/
+export const protectedProcedure = t.procedure.use(isAuthed);
